001514049422debf938502beb8c57c0468efa0b5
boyarko-p <islademyerte@gmail.com>
2020-04-01 12:18:08 +0300

    add fonts

--- app/js/common.js
+++ app/js/common.js
@@ -1,11 +1,1 @@
-//Polyfill object-fit-images
-$(function () {
-  objectFitImages()
-})
-
-//Preloader
-$(window).on('load', function () {
-  var $preloader = $('#preloader')
-  $preloader.delay(500).fadeOut('slow')
-})
 
